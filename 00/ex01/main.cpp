2b403fcf75da3c404b767a38694f055ba04c89ca
Tomas Sierra paradinas <tsierra-@c1r11s4.42madrid.com>
2021-08-04 15:36:57 +0200

    Change GXX as compilation variable and start class

--- 00/ex01/main.cpp
+++ 00/ex01/main.cpp
@@ -1,4 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+class	Contact
+{
+};
+
+class	PhoneBook
+{
+	public:
+		std::array	contact;
+};
+
 int main(void)
 {
+	std::string	cmd;
+
+	while (1)
+	{
+		std::cout << "phonebook: ";
+		std::cin >> cmd;
+		if (cmd.compare("EXIT") == 0)
+			break ;
+		else if (cmd.compare("ADD") == 0)
+			std::cout << "Insert new contact\n";
+		else if (cmd.compare("SEARCH") == 0)
+			std::cout << "Find a registred contact\n";
+	}
 	return (0);
 }
