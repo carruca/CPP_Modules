62f361fa9345f248d544cb2fda586657ea0d7be6
carruca <tomtomsierra@gmail.com>
2021-08-23 23:09:16 +0200

    Modify string to insert new string

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -7,11 +7,18 @@ int	main( int argc, char **argv ) {
 	std::ifstream	ifs(argv[1], std::ifstream::in);
 	std::ofstream	ofs("FILENAME.replace", std::ofstream::out);
 	std::string		nextLine;
+	int				pos = 0;
 
-	if (argc == 2) {
+	if (argc == 4) {
 
 		while (std::getline(ifs, nextLine)) {
 
+			pos = nextLine.find(argv[2]);
+			if (pos != -1) {
+
+				nextLine.erase(pos, strlen(argv[2]));
+				nextLine.insert(pos, argv[3]);
+			}
 			ofs << nextLine;
 			if (!ifs.eof())
 				ofs << std::endl;
