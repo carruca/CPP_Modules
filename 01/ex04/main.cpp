f5dad3ab0886a5aa40b01b87588679538b3d6056
carruca <tomtomsierra@gmail.com>
2021-08-26 17:36:02 +0200

    rename txt file and complete exercise

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -2,27 +2,49 @@
 #include <fstream>
 #include <string>
 
+int	error()
+{
+	std::cout << "Wrong arguments" << std::endl;
+	return 1;
+}
+
 int	main( int argc, char **argv ) {
 
-	std::ifstream	ifs(argv[1], std::ifstream::in);
-	std::ofstream	ofs("FILENAME.replace", std::ofstream::out);
-	std::string		nextLine;
-	int				pos = 0;
+	std::ifstream	ifs;
+	std::string		fileName;
+	std::string		s1;
+	std::string		s2;
+	std::ofstream	ofs;
+	std::string		line;
+	std::size_t		pos = 0;
+	std::size_t		start = 0;
+	std::size_t		len;
 
-	if (argc == 4) {
+	if (argc != 4)
+		return error();
+	fileName = argv[1];
+	s1 = argv[2];
+	s2 = argv[3];
+	ifs.open(fileName, std::ifstream::in);
+	if (ifs.fail())
+		return error();
+	ofs.open(fileName + ".replace", std::ofstream::out);
+	if (ofs.fail())
+		return error();
+	len = s1.size();
+	while (std::getline(ifs, line)) {
 
-		while (std::getline(ifs, nextLine)) {
+		start = 0;
+		pos = 0;
+		while ((pos = line.find(s1, start)) != std::string::npos) {
 
-			pos = nextLine.find(argv[2]);
-			if (pos != -1) {
-
-				nextLine.erase(pos, strlen(argv[2]));
-				nextLine.insert(pos, argv[3]);
-			}
-			ofs << nextLine;
-			if (!ifs.eof())
-				ofs << std::endl;
+			line.erase(pos, len);
+			line.insert(pos, s2);
+			start += pos + len;
 		}
+		ofs << line;
+		if (!ifs.eof())
+			ofs << std::endl;
 	}
 	ifs.close();
 	ofs.close();
