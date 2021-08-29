7364469adb231669095b8395b779fd0f2b1a90a7
carruca <tomtomsierra@gmail.com>
2021-08-29 14:17:47 +0200

    Add lvls array to select action and modify action info

--- 01/ex05/Karen.cpp
+++ 01/ex05/Karen.cpp
@@ -18,30 +18,33 @@ Karen::~Karen( void ) {
 
 void	Karen::complain( std::string level ) {
 
-	if (level == "DEBUG")
-		(this->*f[0])();
-	else if (level == "INFO")
-		(this->*f[1])();
-	else if (level == "WARNING")
-		(this->*f[2])();
-	else if (level == "ERROR")
-		(this->*f[3])();
+	std::string	lvls[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+
+	for (int pos = 0; pos < 4; pos++)
+	{
+		if (lvls[pos] == level)
+			(this->*f[pos])();
+	}
 	return ;
 }
 
 void	Karen::debug( void ) {
 
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love to get extra bacon for my ";
-	std::cout << "7XL-double-cheese-triple-pickle-special-ketchup ";
-	std::cout << "burger. I just love it!";
+	std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger.";
+	std::cout << std::endl;
+	std::cout << "I just love it!";
 	std::cout << std::endl;
 	return ;
 }
 
 void	Karen::info( void ) {
 
-	std::cout << "I cannot believe adding extra bacon ";
-	std::cout << "cost more money. You don’t put enough! ";
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon cost more money.";
+	std::cout << std::endl;
+	std::cout << "You don’t put enough! ";
 	std::cout << "If you did I would not have to ask for it!";
 	std::cout << std::endl;
 	return ;
@@ -49,8 +52,10 @@ void	Karen::info( void ) {
 
 void	Karen::warning( void ) {
 
-	std::cout << "I think I deserve to have some extra bacon ";
-	std::cout << "for free. I’ve been coming here for years ";
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free.";
+	std::cout << std::endl;
+	std::cout << "I’ve been coming here for years ";
 	std::cout << "and you just started working here last month.";
 	std::cout << std::endl;
 	return ;
@@ -58,6 +63,7 @@ void	Karen::warning( void ) {
 
 void	Karen::error( void ) {
 
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable, ";
 	std::cout << "I want to speak to the manager now.";
 	std::cout << std::endl;
