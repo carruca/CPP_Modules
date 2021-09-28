4615ae9a0fc2ed374b942c7329b19f68475cd949
carruca <tomtomsierra@gmail.com>
2021-09-29 01:00:07 +0200

    Add main subject examples

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -3,30 +3,21 @@
 
 int	main( void ) {
 
-	int		a	= 4;
-	int		b	= 2;
-	float	c	= 42.06;
-	float	d	= 34.02;
+	int				a	= 2;
+	int				b	= 3;
 
-	std::cout << "Max number between " << a << " and " << b << " is " <<  max<int>( a, b ) << std::endl;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	std::cout << "Min number between " << a << " and " << b << " is " <<  min<int>( a, b ) << std::endl;
+	std::string		c	= "chaine1";
+	std::string 	d	= "chaine2";
 
-	std::cout << "Swap values " << a << " and " << b << std::endl;
-
-	swap<int>( a , b );
-
-	std::cout << "Now values are " << a << " and " << b << std::endl;
-
-	std::cout << "Max number between " << c << " and " << d << " is " << max<float>( c, d ) << std::endl;
-
-	std::cout << "Min number between " << c << " and " << d << " is " << min<float>( c, d ) << std::endl;
-
-	std::cout << "Swap values " << c << " and " << d << std::endl;
-
-	swap<float>( c , d );
-
-	std::cout << "Now values are " << c << " and " << d << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
 	return 0;
 }
