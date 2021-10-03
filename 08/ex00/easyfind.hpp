79fb28537cd750929f266d62e29560afd2b0f752
carruca <tomtomsierra@gmail.com>
2021-10-03 19:18:42 +0200

    Complete easyfind with find algorithm and add test

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -1,10 +1,28 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-template< typename T >
-int	easyfind( T container, int to_find ) {
+# include <algorithm>
+# include <exception>
 
-	return 0;
+class	OutOfRangeException : public std::exception {
+
+public:
+
+	virtual const char	*what() const throw() {
+
+		return "not found";
+	};
+};
+
+template< typename T >
+typename T::iterator	easyfind( T &container, int to_find ) {
+
+	typename T::iterator	it;
+
+	it = std::find( container.begin(), container.end(), to_find );
+	if ( it == container.end() )
+		throw OutOfRangeException();
+	return it;
 }
 
 #endif
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -1,6 +1,24 @@
 #include "easyfind.hpp"
+#include <vector>
+#include <iostream>
 
 int	main( void ) {
+
+	std::vector<int>			v1( 10, 3 );
+	std::vector<int>::iterator	it;
+	int							to_find;
+
+	to_find = 2;
+
+	try {
+
+		it = easyfind( v1, to_find );
+		std::cout << *it << " found" << std::endl;
+	}
+	catch ( std::exception &e ) {
+
+		std::cout << to_find << " " << e.what() << std::endl;
+	}
 
 	return 0;
 }
